651528e741146025c1f5a3b6260068ca86e04485
B.J. May <bjmay@BJs-MacBook-Pro-2.local>
2021-05-04 17:54:20 -0400

    world automatically takes turns

--- src/world/world.jsx
+++ src/world/world.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function World(props) {
 
   const { damage, action, isTurn } = props
 
+  useEffect(() => {
+    if (isTurn) {
+      action()
+    }
+  })
+
   return (
     <div>
       <div>WORLD (has taken {damage} damage.)</div>
-      <button disabled={!isTurn} onClick={() => action()}>BONK the Player</button>
     </div>
   )
 }

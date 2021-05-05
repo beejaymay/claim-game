7e5f5009db055607516f88bfde4eb746dbe39de5
B.J. May <bjmay@BJs-MacBook-Pro-2.local>
2021-05-04 22:34:47 -0400

    setting up atom structure within component

---
+++ src/game/atoms/index.js
@@ -0,0 +1,3 @@
+import playerCurrency from './playerCurrency'
+
+export { playerCurrency }
---
+++ src/game/atoms/playerCurrency.js
@@ -0,0 +1,6 @@
+import { atom } from 'recoil'
+
+export default playerCurrency = atom({
+  key: 'playerCurrency',
+  default: 0
+})

fdd8fe6f6a0a94a0af1f5ef4fadaabc58808a5eb
B.J. May <bj.may.hea4@statefarm.com>
2021-05-12 15:05:41 -0400

    Basic styles, roughed-in protection concept

--- src/feed/feed.jsx
+++ src/feed/feed.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './feed.css'
 
 export default function Feed(props) {
   const { feed } = props
 
   return (
-    <div>
+    <div className='feed'>
       Here's what happened:
       { feed.map((item, idx) => {
         return <div key={`item${idx}`}>{item}</div>

b43a606d11ce81e89f66266aedfd6cca5d3c4925
B.J. May <beejay.may@gmail.com>
2021-05-12 18:41:58 -0400

    importing individual atoms

--- src/game/game.jsx
+++ src/game/game.jsx
@@ -1,43 +1,18 @@
 import React from 'react'
-import { useRecoilState, atom } from 'recoil'
+import { useRecoilState } from 'recoil'
 
 import Player from '../player/player'
 import World from '../world/world'
 import Feed from '../feed/feed'
 
-import { policies } from './atoms/policies'
-
-const _worldDamage = atom({
-  key: 'worldDamage',
-  default: 0
-})
-
-const _playerDamage = atom({
-  key: 'playerDamage',
-  default: 0
-})
-
-const _isPlayerTurn = atom({
-  key: 'isPlayerTurn',
-  default: true
-})
-
-const _isWorldTurn = atom ({
-  key: 'isWorldTurn',
-  default: false
-})
-
-const _feed = atom({
-  key: 'feed',
-  default: []
-})
+import { policies, worldDamage, playerDamage, isWorldTurn, isPlayerTurn, feed } from './atoms'
 
 export default function Game() {
-  const [playerDamage, setPlayerDamage] = useRecoilState(_playerDamage)
-  const [worldDamage, setWorldDamage] = useRecoilState(_worldDamage)
-  const [isPlayerTurn, setIsPlayerTurn] = useRecoilState(_isPlayerTurn)
-  const [isWorldTurn, setIsWorldTurn] = useRecoilState(_isWorldTurn)
-  const [feed, setFeed] = useRecoilState(_feed)
+  const [_playerDamage, setPlayerDamage] = useRecoilState(playerDamage)
+  const [_worldDamage, setWorldDamage] = useRecoilState(worldDamage)
+  const [_isPlayerTurn, setIsPlayerTurn] = useRecoilState(isPlayerTurn)
+  const [_isWorldTurn, setIsWorldTurn] = useRecoilState(isWorldTurn)
+  const [_feed, setFeed] = useRecoilState(feed)
   const [_policies, setPolicies] = useRecoilState(policies)
 
   const randomDamage = () => {
@@ -48,10 +23,10 @@ export default function Game() {
     let dmg = randomDamage() + 10
     let didHit = (Math.random() > 0.5)
     if (didHit) {
-      setWorldDamage(worldDamage + dmg)
-      setFeed([...feed, `Player CLOBBERED World for ${dmg} damage!`])
+      setWorldDamage(_worldDamage + dmg)
+      setFeed([..._feed, `Player CLOBBERED World for ${dmg} damage!`])
     } else {
-      setFeed([...feed, `Player tried to CLOBBER World, but missed!`])
+      setFeed([..._feed, `Player tried to CLOBBER World, but missed!`])
     }
     setIsPlayerTurn(false)
     setTimeout(() => {
@@ -61,8 +36,8 @@ export default function Game() {
 
   const bonkWorld = () => {
     let dmg = randomDamage()
-    setWorldDamage(worldDamage + dmg)
-    setFeed([...feed, `Player bonked World for ${dmg} damage.`])
+    setWorldDamage(_worldDamage + dmg)
+    setFeed([..._feed, `Player bonked World for ${dmg} damage.`])
     setIsPlayerTurn(false)
     setTimeout(() => {
       setIsWorldTurn(true)
@@ -73,8 +48,8 @@ export default function Game() {
     let protection = _policies[0] ? _policies[0].protection : 0;
     let dmg = randomDamage() - protection
     dmg = dmg < 0 ? 0 : dmg
-    setPlayerDamage(playerDamage + dmg)
-    setFeed([...feed, `World bonked Player for ${dmg} damage.${protection > 0 ? " (protected from " + protection + " damage)" : ""}`])
+    setPlayerDamage(_playerDamage + dmg)
+    setFeed([..._feed, `World bonked Player for ${dmg} damage.${protection > 0 ? " (protected from " + protection + " damage)" : ""}`])
     setIsWorldTurn(false)
     setIsPlayerTurn(true)
   }
@@ -84,7 +59,7 @@ export default function Game() {
       type: 'homeowners',
       protection: 5
     }])
-    setFeed([...feed, `Player bought a 5 protection policy.`])
+    setFeed([..._feed, `Player bought a 5 protection policy.`])
     setIsPlayerTurn(false)
     setTimeout(() => {
       setIsWorldTurn(true)
@@ -93,9 +68,9 @@ export default function Game() {
 
   return (
     <>
-      <Player damage={playerDamage} addPolicy={addPolicy} bonk={bonkWorld} clobber={clobberWorld} isTurn={isPlayerTurn} />
-      <World damage={worldDamage} bonk={bonkPlayer} isTurn={isWorldTurn} />
-      <Feed feed={feed} />
+      <Player damage={_playerDamage} addPolicy={addPolicy} bonk={bonkWorld} clobber={clobberWorld} isTurn={_isPlayerTurn} />
+      <World damage={_worldDamage} bonk={bonkPlayer} isTurn={_isWorldTurn} />
+      <Feed feed={_feed} />
     </>
   )
 }
